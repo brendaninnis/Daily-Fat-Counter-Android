c9f024c34f5b6ebf0d270739909e334bef442e0e
Brendan Innis <innisbrendan@gmail.com>
2022-05-03 07:59:09 -0700

    basic counter circle drawing complete. bugged as hell XD

--- app/src/main/java/ca/brendaninnis/dailyfatcounter/view/CircularCounter.kt
+++ app/src/main/java/ca/brendaninnis/dailyfatcounter/view/CircularCounter.kt
@@ -1,27 +1,50 @@
 package ca.brendaninnis.dailyfatcounter.view
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.ContextCompat
+import ca.brendaninnis.dailyfatcounter.R
 
 class CircularCounter(context: Context, attrs: AttributeSet) : View(context, attrs) {
-    // Paint object for coloring and styling
-    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
-    // Some colors for the face background, eyes and mouth.
-    private var faceColor = Color.YELLOW
-    private var eyesColor = Color.BLACK
-    private var mouthColor = Color.BLACK
-    private var borderColor = Color.BLACK
-    // Face border width in pixels
-    private var borderWidth = 4.0f
-    // View size in pixels
-    private var size = 320
+    private var rectF = RectF(0f, 0f, 0f, 0f)
+    private val thiccness = context.resources.getDimension(R.dimen.counterThiccness)
+    private val halfThiccness = thiccness * 0.5f
+    private val thirdThiccness = thiccness * 0.33f
+    private val sixthThiccness = thirdThiccness * 0.5f
+    private val innerInset = sixthThiccness + halfThiccness
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.STROKE
+        strokeCap = Paint.Cap.ROUND
+    }
+    private val colors = intArrayOf(ContextCompat.getColor(context, R.color.green),
+        ContextCompat.getColor(context, R.color.yellow),
+        ContextCompat.getColor(context, R.color.yellow),
+        ContextCompat.getColor(context, R.color.green))
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        rectF = RectF(0f, 0f, w.toFloat(), h.toFloat())
+        val gradient = SweepGradient(w * 0.5f, h * 0.5f, colors, null)
+        paint.shader = gradient
+    }
 
     override fun onDraw(canvas: Canvas) {
         // call the super method to keep any drawing from the parent side.
         super.onDraw(canvas)
+        drawProgressCircle(canvas)
+        drawInnerCircle(canvas)
+    }
+
+    private fun drawInnerCircle(canvas: Canvas) {
+        paint.strokeWidth = thirdThiccness
+        canvas.drawOval(rectF, paint)
+    }
+
+    private fun drawProgressCircle(canvas: Canvas) {
+        rectF.inset(halfThiccness, halfThiccness)
+        paint.strokeWidth = thiccness
+        canvas.drawArc(rectF, 0f, 105f, false, paint)
     }
 }
